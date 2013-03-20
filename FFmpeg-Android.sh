7861716b7eb177858449fa36bd2d77d0bdaab93b
Cedric Fung <cedric@vec.io>
2013-03-20 09:52:10 +0800

    Stick to the FFmpeg's git revision `ffmpeg-version`

--- FFmpeg-Android.sh
+++ FFmpeg-Android.sh
@@ -1,35 +1,4 @@
 #!/bin/bash
-#
-# FFmpeg-Android, a bash script to build FFmpeg for Android.
-#
-# Copyright (c) 2012 Cedric Fung <wolfplanet@gmail.com>
-#
-# FFmpeg-Android will build FFmpeg for Android automatically,
-# with patches from VPlayer's Android version <https://vplayer.net/>.
-#
-# FFmpeg-Android is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 3 of the License, or (at your option) any later version.
-
-# FFmpeg-Android is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-
-# You should have received a copy of the GNU Lesser General Public
-# License along with FFmpeg-Android; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-#
-#
-# Instruction:
-#
-# 0. Install git and Android ndk
-# 1. $ export ANDROID_NDK=/path/to/your/android-ndk
-# 2. $ ./FFmpeg-Android.sh
-# 3. libffmpeg.so will be built to build/ffmpeg/{neon,armv7,vfp,armv6}/
-#
-#
 
 DEST=`pwd`/build/ffmpeg && rm -rf $DEST
 SOURCE=`pwd`/ffmpeg
@@ -38,10 +7,16 @@ if [ -d ffmpeg ]; then
   cd ffmpeg
 else
   git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg
-  cd ffmpeg && patch -p1 <../FFmpeg-VPlayer.patch
+  cd ffmpeg
 fi
 
-git log | head -n 1 | awk '{print $2}' > ../ffmpeg-version
+git reset --hard
+git clean -f -d
+git checkout `cat ../ffmpeg-version`
+patch -p1 <../FFmpeg-VPlayer.patch
+[ $PIPESTATUS == 0 ] || exit 1
+
+git log --pretty=format:%H -1 > ../ffmpeg-version
 
 TOOLCHAIN=/tmp/vplayer
 SYSROOT=$TOOLCHAIN/sysroot/
@@ -73,20 +48,22 @@ FFMPEG_FLAGS="--target-os=linux \
   --disable-ffserver \
   --disable-avdevice \
   --disable-avfilter \
-  --disable-encoders  \
   --disable-encoders \
   --disable-muxers \
-  --disable-bsfs \
   --disable-filters \
   --disable-devices \
   --disable-everything \
   --enable-protocols  \
   --enable-parsers \
   --enable-demuxers \
-  --disable-demuxer=sbg \
   --enable-decoders \
+  --enable-bsfs \
   --enable-network \
   --enable-swscale  \
+  --disable-demuxer=sbg \
+  --disable-demuxer=dts \
+  --disable-parser=dca \
+  --disable-decoder=dca \
   --enable-asm \
   --enable-version3"
 
